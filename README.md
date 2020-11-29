0355c8aab0e81e83f5ce4091a034f394a6baee29
Bea <beatriztattooer@gmail.com>
2020-11-29 20:06:22 -0300

    CATS: first commit It renders a cat when you click the button and added a new cat name at the list. It's ugly, but honest work

---
+++ README.md
@@ -0,0 +1,14 @@
+# Random cats
+## This project shows random cats
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+#### Thank you for your support!
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="row">
+
+    <div class="col d-inline-block">
+      <input v-model="cats.newCat" placeholder="Escreva o nome de um gatinho aqui :D" />
+      <button v-on:click="addKitty" class="btn w-100">Adicionar gatinho</button>
+  
+      <ul>
+        <li v-for="(cat, index) in cats.list" :key="index">{{ cat.name }}</li>
+      </ul>
+    </div>
+
+    <div class="col d-inline-block">
+      <button @click.stop.prevent="getCats()" class="btn w-100">Clique para ver um gato</button>
+      <div v-for="(cat, index) in cats.api" :key="index">
+          <img :src="cat.url" alt="" class="cat-image"/>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  data(){
+    return{
+      cats: {
+        api: [],
+        newCat: '',
+        list: [
+            { name: 'Romeo' },
+            { name: 'Luiza' },
+            { name: 'Henry' }
+        ]
+      },
+      loader: {
+        cats: false,
+      },
+      response: {
+          status: "",
+          msg: "Ooops! Houve um erro, tente novamente mais tarde."
+      }
+    }
+  },
+  methods: {
+    getCats() {
+      this.loader.cats = true;
+
+      var url = 'https://api.thecatapi.com/v1/images/search';
+      var vm = this;
+      axios.get(url).then(function (response) {
+          vm.cats.api = response.data;
+          vm.loader.cats = false;
+      });
+    },
+    addKitty: function () {
+      return this.cats.list.push({ name: this.cats.newCat })
+    }
+  }
+}
+</script>
+
+<style>
+  .row{
+    width: 800px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  .col{
+    width: 50%;
+    margin: 1rem;
+  }
+  .d-inline-block{
+    display: inline-block;
+  }
+  .w-100{
+    width: 100%;
+  }
+  input{
+    padding: 10px 20px;
+    width: -webkit-fill-available;
+  }
+  .btn{
+    padding: 10px 20px;
+    background-color: black;
+    color: white;
+  }
+  .cat-image{
+    width: 100%;
+    height: auto;
+  }
+</style>
