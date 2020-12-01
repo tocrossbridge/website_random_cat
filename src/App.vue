97b736b157452e1c6af03893143757a6e90d0c62
Bea <beatriztattooer@gmail.com>
2020-11-30 22:57:17 -0300

    CLEAN CODE: rename file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RandomCat msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RandomCat from './components/RandomCat.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RandomCat
   }
 }
 </script>
